1a0b0c32767e711ed855ca26e0585d911000070b
Maya-Mostafa <mayamostafa@gmail.com>
2022-02-24 08:21:19 -0500

    intial setup -working

--- src/webparts/spFxCarousel/components/SpFxCarousel.tsx
+++ src/webparts/spFxCarousel/components/SpFxCarousel.tsx
@@ -1,25 +1,51 @@
-import * as React from 'react';
-import styles from './SpFxCarousel.module.scss';
-import { ISpFxCarouselProps } from './ISpFxCarouselProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import styles from "./SpFxCarousel.module.scss";
+import { ISpFxCarouselProps } from "./ISpFxCarouselProps";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-export default class SpFxCarousel extends React.Component<ISpFxCarouselProps, {}> {
-  public render(): React.ReactElement<ISpFxCarouselProps> {
-    return (
-      <div className={ styles.spFxCarousel }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+export default function SpFxCarousel(props: ISpFxCarouselProps) {
+	const customRenderThumb = (children) => {
+		children = [
+			"Peel Virtual Secondary School",
+			"Black History Month",
+			"2021-22 Parents Reaching Out (PRO) Grants",
+			"Start with a Smile",
+		];
+		return children.map((item) => {
+			return <span>{item}</span>;
+		});
+	};
+
+	return (
+		<Carousel
+			centerMode
+			centerSlidePercentage={80}
+			infiniteLoop
+			renderThumbs={customRenderThumb}
+		>
+			<div>
+				<img
+					src={require(`../../../Assets/Peel Virtual Secondary School.png`)}
+				/>
+				<p className='legend'>Legend 1</p>
+			</div>
+			<div>
+				<img
+					src={require(`../../../Assets/Black History Month MySite Spotlight.png`)}
+				/>
+				<p className='legend'>Legend 2</p>
+			</div>
+			<div>
+				<img src={require(`../../../Assets/PRO_Grant_Banner.png`)} />
+				<p className='legend'>Legend 3</p>
+			</div>
+			<div>
+				<img
+					src={require(`../../../Assets/Kindergarten Registration Online.png`)}
+				/>
+				<p className='legend'>Legend 3</p>
+			</div>
+		</Carousel>
+	);
 }
